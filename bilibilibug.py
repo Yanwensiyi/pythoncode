a948e68d9904080471d519d0ef7dd6446fbc9a8d
Yanwensiyi <53609726+Yanwensiyi@users.noreply.github.com>
2019-08-04 22:23:47 +0800

    Update bilibilibug.py

--- bilibilibug.py
+++ bilibilibug.py
@@ -8,20 +8,32 @@ class biliuser:
     self.update(uid)
     return
   def update(self,uid):
-    data=loads(u.urlopen("https://api.bilibili.com/x/space/acc/info?mid="+str(uid)).read().decode("utf-8"))["data"]
-    self.mid=str(uid)
-    self.name=data["name"]
-    self.sex=data["sex"]
-    self.sign=data["sign"]
-    self.level=data["level"]
-    self.birthday=data["birthday"]
+    try:
+      data=loads(u.urlopen("https://api.bilibili.com/x/space/acc/info?mid="+str(uid)).read().decode("utf-8"))["data"]
+      self.mid=str(uid)
+      self.name=data["name"]
+      self.sex=data["sex"]
+      self.sign=data["sign"]
+      self.level=data["level"]
+      self.birthday=data["birthday"]
+      self.vain=False
+      pass
+    except:
+      self.vain=True
+      pass
     return
   pass
 file = open("biliusers.txt","w")
-for x in range(1,21):
+for x in range(1,1001):
   i = biliuser(x)
   users.append(i)
-  print("UID:%s\nName:%s\nGender:%s\nSign:%s\nLevel:%s\nBirthday:%s\n\n"%(i.mid,i.name,i.sex,i.sign,i.level,i.birthday))
-  file.write("UID:%s\nName:%s\nGender:%s\nSign:%s\nLevel:%s\nBirthday:%s\n\n"%(i.mid,i.name,i.sex,i.sign,i.level,i.birthday))
+  try:
+    if i.vain==True:
+      continue
+    print("UID:%s\nName:%s\nGender:%s\nSign:%s\nLevel:%s\nBirthday:%s\n\n"%(i.mid,i.name,i.sex,i.sign,i.level,i.birthday))
+    file.write("UID:%s\nName:%s\nGender:%s\nSign:%s\nLevel:%s\nBirthday:%s\n\n"%(i.mid,i.name,i.sex,i.sign,i.level,i.birthday))
+    pass
+  except:
+    pass
   pass
 file.close()
